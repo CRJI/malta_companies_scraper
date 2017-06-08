3cb1877189c5acf4e19a46411a1e77cd1258fa29
Roland Stefani <roland.stefani@rocketmail.com>
2017-06-08 09:43:45 +0300

    Added the test_set to the repo so I don't have to rebuild it every time I need it.

--- malta_roc_lookup.py
+++ malta_roc_lookup.py
@@ -8,6 +8,10 @@ from bs4 import BeautifulSoup
 from malta_roc_gather2 import generate_extracted_data
 
 
+_BASE_URL = ''
+_LOGIN_URL = ''
+
+def generate_
 def main():
 
     for entity in generate_extracted_data():
